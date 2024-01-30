94f1766e50798883637a210207292a4c88d713e3
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2024-01-30 22:07:58 +0000

    changes to endpoint

--- src/utilities/API/fetchTaskDetails.js
+++ src/utilities/API/fetchTaskDetails.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function fetchTaskDetails(taskId) {
     try {
-        const returnedTask = await axios.get(`https://demonstration.swiftcase.co.uk/api/v2/kntlepq8watc93klowswd1tgdnopccm3/task/${taskId}.json`)
+        const returnedTask = await axios.get(`${import.meta.env.VITE_APP_API_URL}${import.meta.env.VITE_APP_API_KEY}/task/${taskId}.json`)
         return returnedTask.data
     } catch (error) {
         throw error
